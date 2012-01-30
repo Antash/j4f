f4eeb500cc326d9deb430eaeed9c2f590401e9d5
Anton Ashmarin <aashmarin@gmail.com>
2012-01-30 09:34:12 +0000

    [tests] railway added

---
+++ railway/railway/Mesh.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace railway
+{
+	/// <summary>
+	/// Railway mesh. Contains list of railway lines
+	/// </summary>
+	internal class Mesh
+	{
+		private readonly List<Line> _meshLines;
+
+		public Mesh()
+		{
+			_meshLines = new List<Line>();
+		}
+
+		/// <summary>
+		/// Addition of the new line.
+		/// </summary>
+		/// <param name="newLine">New railway line</param>
+		/// <returns>Returns true if addition was successfully completed</returns>
+        /// <remarks>You can't add degenerate line or line witch was added earlier</remarks>
+		public bool Add(Line newLine)
+		{
+			bool readyToAdd = newLine.StationID1 != newLine.StationID2 && !_meshLines.Contains(newLine);
+			if (readyToAdd)
+				_meshLines.Add(newLine);
+			return readyToAdd;
+		}
+
+		/// <summary>
+		/// Checking mesh path
+		/// </summary>
+		/// <param name="path">Path to check</param>
+        /// <returns>Returns true if the path is connected</returns>
+		/// <remarks>If path contains only one station it is exists.</remarks>
+		public bool IsPathValid(uint[] path)
+		{
+			var result = true;
+
+			for (var i = 0; i < path.Length - 1; i++)
+			{
+				var tempMesh = _meshLines.FindAll(line => line.StationID1 == path[i] || line.StationID2 == path[i]);
+				if (tempMesh.Count != 0)
+					tempMesh = tempMesh.FindAll(line => line.StationID2 == path[i + 1] || line.StationID1 == path[i + 1]);
+				if (tempMesh.Count == 0)
+				{
+					result = false;
+					break;
+				}
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Counts distance to each station of the path from the beginning to end
+		/// </summary>
+		/// <param name="path">Path to calculate</param>
+		/// <exception cref="ArgumentException">Occures if input path is invalid (empty or unconnected)</exception>
+		/// <returns>K/V pairs contains Station/Distance to it</returns>
+		public KeyValuePair<uint, uint>[] CalcPath(uint[] path)
+		{
+			if (path.Length == 0)
+				throw new ArgumentException("Path contains no stations!");
+			if (IsPathValid(path) == false)
+				throw new ArgumentException("Path does not valid in the current mesh!");
+
+			var result = new KeyValuePair<uint, uint>[path.Length];
+			result[0] = new KeyValuePair<uint, uint>(path[0], 0);
+
+			for (var i = 0; i < path.Length - 1; i++)
+			{
+				var line = _meshLines.Find(l =>
+					l.StationID1 == path[i] && l.StationID2 == path[i + 1] ||
+					l.StationID2 == path[i] && l.StationID1 == path[i + 1]);
+				result[i + 1] = new KeyValuePair<uint, uint>(path[i + 1], result[i].Value + line.Length);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ railway/railway/Railway.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace railway
+{
+    /// <summary>
+    /// Railway class.
+    /// </summary>
+    class Railway
+    {
+        private readonly Mesh _railwayMesh;
+        private readonly List<uint[]> _paths;
+        // K/V pairs contains (Paths station)/(Distance to it from the beginning of the path)
+        private readonly List<KeyValuePair<uint, uint>[]> _pathCalculations;
+
+        private Railway()
+        {
+            _railwayMesh = new Mesh();
+            _paths = new List<uint[]>();
+            _pathCalculations = new List<KeyValuePair<uint, uint>[]>();
+        }
+
+        public Railway(Mesh mesh)
+            : this()
+        {
+            _railwayMesh = mesh;
+        }
+
+        /// <summary>
+        /// Adds new path or nothing if new path is invalid
+        /// </summary>
+        /// <param name="newPath">New path to add</param>
+        /// <returns>Addition success result</returns>
+        public bool AddPath(uint[] newPath)
+        {
+            var readyToAdd = _railwayMesh.IsPathValid(newPath);
+            if (readyToAdd)
+            {
+                _paths.Add(newPath);
+                _pathCalculations.Add(_railwayMesh.CalcPath(newPath));
+            }
+            return readyToAdd;
+        }
+
+        /// <summary>
+        /// Clears all added paths
+        /// </summary>
+        public void ClearPaths()
+        {
+            _paths.Clear();
+            _pathCalculations.Clear();
+        }
+
+        /// <summary>
+        /// Function to detect crash availability
+        /// </summary>
+        /// <returns>Returns true if there is no chash detected</returns>
+        public bool IfCrashExists()
+        {
+            // Consider each distinct pair of paths
+            for (int pi1 = 0; pi1 < _pathCalculations.Count; pi1++)
+            {
+                var pathCalc1 = _pathCalculations[pi1];
+                for (int pi2 = pi1 + 1; pi2 < _pathCalculations.Count; pi2++)
+                {
+                    var pathCalc2 = _pathCalculations[pi2];
+                    for (int i = 0, j = 0; i < pathCalc1.Length && j < pathCalc2.Length; )
+                    {
+                        // paths pass through one station
+                        if (pathCalc1[i].Key == pathCalc2[j].Key)
+                        {
+                            // trains are on the station together - crash
+                            if (pathCalc1[i].Value == pathCalc2[j].Value)
+                                return true;
+                            // check for the crash on the same line
+                            if (Comp(pathCalc1, i, pathCalc2, j) ||
+                                Comp(pathCalc2, j, pathCalc1, i))
+                                return true;
+                        }
+
+                        // extend the path that is shorter
+                        if (pathCalc1[i].Value <= pathCalc2[j].Value)
+                            i++;
+                        else
+                            j++;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool Comp(IList<KeyValuePair<uint, uint>> pathCalc1, int i, IList<KeyValuePair<uint, uint>> pathCalc2, int j)
+        {
+            return
+                // changed indexes must not be out of range
+                i + 1 < pathCalc1.Count && j - 1 >= 0 &&
+                // previous station of path 1 must be a next station of path2
+                pathCalc1[i + 1].Key == pathCalc2[j - 1].Key &&
+                // the first train must reach station before the second leaves it 
+                pathCalc1[i].Value <= pathCalc2[j].Value;
+        }
+    }
+}
